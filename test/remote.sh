c6a19049dca80c5307a0dc89a1f5b41a4ad7d984
Joachim Nilsson <troglobit@gmail.com>
2019-10-31 12:07:52 +0100

    Trim remote test, restore startup/log `sleep 2`
    Even on my laptop it sometimes takes a bit too long for tshark to start
    up and syslogd to actually FWD the $MSG to remote.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- test/remote.sh
+++ test/remote.sh
@@ -4,20 +4,23 @@ set -ex
 
 MSG="kilroy"
 
-/sbin/ifconfig -a
-
+# Start collector in background, note: might need sudo!
 tshark -Qni lo -w ${CAP} port ${PORT} &
-#tcpdump -qlni lo -w ${CAP} port ${PORT} &
 PID="$!"
-sleep 1
-ps fax  |grep -A3 tshark
+
+# Wait for tshark to start up properly
+sleep 2
 
 ../src/logger -u ${SOCK} ${MSG}
 
-sleep 1
+# Wait for any OS delays, in particular on Travis
+sleep 2
+
+# Stop tshark collector
 kill -TERM ${PID}
 wait ${PID}
 
+# Analyze content, should have $MSG now ...
 tshark -d udp.port==${PORT},syslog -r ${CAP} | grep ${MSG}
 rm ${CAP}
 
