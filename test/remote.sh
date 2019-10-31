cd7d2ca0be15751e4afcf44cbbd39300486bdaa0
Joachim Nilsson <troglobit@gmail.com>
2019-10-31 10:35:19 +0100

    sudo do what I mean
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- test/remote.sh
+++ test/remote.sh
@@ -6,8 +6,8 @@ MSG="kilroy"
 
 /sbin/ifconfig -a
 
-tshark -Qni lo -w ${CAP} port ${PORT} &
-#tcpdump -qlni lo -w ${CAP} port ${PORT} &
+sudo tshark -Qni lo -w ${CAP} port ${PORT} &
+#sudo tcpdump -qlni lo -w ${CAP} port ${PORT} &
 PID="$!"
 sleep 5
 ps fax  |grep -A3 tshark
@@ -15,7 +15,7 @@ ps fax  |grep -A3 tshark
 ../src/logger -u ${SOCK} ${MSG}
 
 sleep 5
-kill -TERM ${PID}
+sudo kill -TERM ${PID}
 wait ${PID}
 
 tshark -d udp.port==${PORT},syslog -r ${CAP} | grep ${MSG}
