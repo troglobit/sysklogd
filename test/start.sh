62c651a9794f08caf8424d024226f69301cfa90f
Joachim Nilsson <troglobit@gmail.com>
2019-11-02 11:47:24 +0100

    Adapt unit tests to leverage and test /etc/syslog.d/*.conf support
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- test/start.sh
+++ test/start.sh
@@ -5,8 +5,19 @@ if [ x"${srcdir}" = x ]; then
     srcdir=.
 fi
 
+mkdir -p ${CONFD}
 cat <<EOF > ${CONF}
+# Nothing here yo
+include ${CONFD}/*.conf
+EOF
+
+cat <<EOF > ${CONFD}/foo.conf
+# Local log file, avoid sync to disk
 *.*	-${LOG}
+EOF
+
+cat <<EOF > ${CONFD}/bar.conf
+# For remote logging
 *.*	@127.0.0.2
 EOF
 
