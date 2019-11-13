d6370aeb1f22f99b8d877971060241edebc369ff
Joachim Nilsson <troglobit@gmail.com>
2019-11-13 08:31:36 +0100

    Follow-up to 69608f7: Update API test with TAG formatting change
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- test/api.sh
+++ test/api.sh
@@ -9,7 +9,7 @@ export MSG="no-openlog-apitest"
 
 echo "= Phase 1 - simple syslog(), no openlog() ==============="
 ./api
-grep "api ${MSG}" ${LOG}
+grep "api: ${MSG}" ${LOG}
 
 echo "= Phase 2 - syslog() with openlog() & custom facility ==="
 cat <<EOF >${CONFD}/console.conf
@@ -19,11 +19,11 @@ kill -HUP `cat ${PID}`
 sleep 2
 
 ./api -i foo
-grep "foo ${MSG}" ${LOGCONS}
+grep "foo: ${MSG}" ${LOGCONS}
 
 echo "= Phase 3 - Verify setlogmask() filters out LOG_INFO ===="
 ./api -i xyzzy -l
-grep "xyzzy ${MSG}" ${LOGCONS} || true
+grep "xyzzy: ${MSG}" ${LOGCONS} || true
 
 echo "= Phase 4 - Verify RFC5424 API with syslogp() ==========="
 cat <<EOF >${CONFD}/v1.conf
