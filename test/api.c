e2f077da3f9c2775b4f4d546b21d3a2f648e0633
Joachim Wiberg <troglobit@gmail.com>
2024-12-09 19:12:09 +0100

    test: document api test tool
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/api.c
+++ test/api.c
@@ -1,3 +1,22 @@
+/*
+ * Without any options the standard syslog(3) API is used with severity
+ * LOG_NOTICE without calling openlog(), so the default LOG_USER facility
+ * is used.
+ *
+ * Options:
+ *  -i ID   Log using the given identity to LOG_CONSOLE, calls openlog()
+ *  -l      Set logmask to LOG_NOTICE and log with LOG_INFO
+ *  -p      Use modern syslogp() API and log to LOG_FTP, use with -i ID
+ *
+ * See the -i option for made-easy 'grep' check of the log.
+ *
+ * When the -l option is used, setlogmask() is set to LOG_NOTICE and the
+ * LOG_CONSOLE facility is used for logging.  The latter is a sysklogd
+ * (BSD) specific facility.
+ *
+ * The -p option triggers the use of the modern syslogp() API and sets
+ * the facility to LOG_FTP.
+ */
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
