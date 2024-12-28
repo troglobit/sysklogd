21017ae05375039b8fba53ee11a14539b340c3e1
Joachim Wiberg <troglobit@gmail.com>
2024-12-28 12:44:59 +0100

    test: relocate sighup from remote.sh to separate test
    Previously used in remote.sh as a delay while tshark was starting up.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ test/sighup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Verify syslogd can survive a few SIGHUP's
+# shellcheck disable=SC1090
+if [ -z "${srcdir}" ]; then
+    srcdir=.
+fi
+. "${srcdir}/lib.sh"
+
+verify_logging()
+{
+    MSG="$*"
+    logger "${MSG}"
+    grep "${MSG}" "${LOG}"
+}
+
+rattle_cage()
+{
+    for _ in $(seq "$1"); do
+	dprint "Shaky shaky ..."
+	reload
+    done
+}
+
+run_step "Set up local syslog daemon" setup -m0
+run_step "Verify before shakeup"      verify_logging "Diving in the Sky"
+run_step "Shake it up mister ..."     rattle_cage 5
+run_step "Verify after shakeup"       verify_logging "Summerdream"
