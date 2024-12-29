9b4d8f463303cf01641809bad90332428eca7593
Joachim Wiberg <troglobit@gmail.com>
2024-12-29 01:16:15 +0100

    test: refactor logger.sh to use run_step()
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/logger.sh
+++ test/logger.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 # Verify logger capabilities, for now just remote logger
-# shellcheck disable=SC1090
-#set -x
 if [ -z "${srcdir}" ]; then
     srcdir=.
 fi
 . "${srcdir}/lib.sh"
 
-ip link set lo up
+setup_loopback()
+{
+    ip link set lo up
+}
 
-print "Starting stand-alone logger test ..."
-cap_start
-logger -b -H "$(basename "$0")" -h 127.0.0.3 -I $$ -t test1 "Kilroy was here"
-cap_stop
+verify_logger()
+{
+    cap_start
+    logger -b -H "$(basename "$0")" -h 127.0.0.3 -I $$ -t test1 "Kilroy was here"
+    cap_stop
 
-# Check for the composed BSD procname{PID] syntax
-STR="test1\[$$\]"
-cap_find "$STR" || FAIL "Cannot find: $STR"
+    # Check for the composed BSD procname{PID] syntax
+    STR="test1\[$$\]"
+    cap_find "$STR"
+}
 
-OK
+run_step "Set up loopback interface"                    setup_loopback
+run_step "Verify remote syslog with stand-alone logger" verify_logger
