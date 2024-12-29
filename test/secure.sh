89f50b922381f243552e21efd36bc2c0135a0f92
Joachim Wiberg <troglobit@gmail.com>
2024-12-29 16:31:48 +0100

    test: refactor secure.sh to use run_step()
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/secure.sh
+++ test/secure.sh
@@ -2,8 +2,6 @@
 # Verify secure_mode changes at runtime w/o having to restart syslogd.
 # We want to ensure goint from most secure, to no security, and back,
 # works as intended.
-#
-# shellcheck disable=SC1090
 if [ -z "${srcdir}" ]; then
     srcdir=.
 fi
@@ -11,60 +9,84 @@ fi
 
 MSG="Kilroy was here"
 
+#
+# Helper functions
+#
+
 set_secure_mode()
 {
-	cat <<-EOF > "${CONF}"
-		*.* @127.0.0.2
-		secure_mode=$1
-		EOF
-	if is_running; then
-		reload
-	else
-		setup -m0
-	fi
-	sleep 1
+    cat <<-EOF > "${CONF}"
+	*.* @127.0.0.2
+	secure_mode=$1
+	EOF
+
+    if is_running; then
+	reload
+    else
+	setup -m0
+    fi
+
+    sleep 1			# Wait for any OS delays
 }
 
 do_port_check()
 {
-	netstat -atnup | grep "$PORT\|PORT2"
+    dprint "Checking for port $PORT|$PORT2 ..."
+    netstat -atnup | grep "$PORT\|$PORT2"
 }
 
 check_no_port_open()
 {
-	do_port_check && FAIL "$@"
+    do_port_check || return 0
 }
 
 check_port_open()
 {
-	do_port_check || FAIL "$@"
+    do_port_check
 }
 
 check_remote_logging()
 {
-	cap_start
-	logger "$MSG"
-	cap_stop
-	cap_find "$MSG" || FAIL "Cannot find: $MSG"
+    cap_start
+    logger   "$MSG"
+    cap_stop
+
+    cap_find "$MSG"
 }
 
-print "Secure mode 2 - no remote no ports"
-set_secure_mode 2
-check_no_port_open "Secure mode 2, yet ports are opened!"
+#
+# Test steps
+#
 
-print "Secure mode 1 - remote but no ports"
-set_secure_mode 1
-check_no_port_open "Secure mode 1, yet ports are opened!"
-check_remote_logging
+verify_secure_daemon()
+{
+    set_secure_mode 2
+    check_no_port_open
+}
 
-print "Secure mode 0 - remote and open ports"
-set_secure_mode 0
-check_remote_logging "Secure mode 0, but no ports open!"
-check_port_open
+verify_safe_daemon()
+{
+    set_secure_mode 1
+    check_no_port_open
+}
 
-print "Secure mode 1 - remote but no ports"
-set_secure_mode 1
-check_no_port_open "Secure mode 1, yet ports are opened!"
-check_remote_logging
+verify_default_daemon()
+{
+    set_secure_mode 0
+    check_port_open
+}
 
-OK
+#
+# Run test steps
+#
+
+run_step "Verify secure mode 2 - no remote no ports" verify_secure_daemon
+
+run_step "Verify secure mode 1 - remote, no ports"   verify_safe_daemon
+run_step "Verify                 remote logging"     check_remote_logging
+
+run_step "Verify secure mode 0 - remote, open ports" verify_default_daemon
+run_step "Verify                 remote logging"     check_remote_logging
+
+run_step "Verify secure mode 1 - remote, no ports"   verify_safe_daemon
+run_step "Verify                 remote logging"     check_remote_logging
