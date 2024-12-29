2538f39b1252e3956690008867683d82cfd6ecd5
Joachim Wiberg <troglobit@gmail.com>
2024-12-29 13:58:23 +0100

    test: refactor memleak.sh to use run_step()
    Also, add more stimuli and actually verify output from valgrind.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/memleak.sh
+++ test/memleak.sh
@@ -1,30 +1,42 @@
 #!/bin/sh
 # Start, SIGHUP, and log a run of syslogd under Valgrind
-# shellcheck disable=SC1090
-#set -x
 if [ -z "${srcdir}" ]; then
     srcdir=.
 fi
-
-# shellcheck disable=SC2034
-VALGRIND="valgrind --leak-check=full --show-leak-kinds=all"
 . "${srcdir}/lib.sh"
 
-# Only needed for verifying correct RFC3164 parsing
-cat <<-EOF >"${CONFD}/99-wall.conf"
+setup_valgrind()
+{
+    VALGRIND="valgrind --leak-check=full --show-leak-kinds=all"
+    DEBUG=true
+
+    # Only needed for verifying correct RFC3164 parsing
+    cat <<-EOF >"${CONFD}/99-wall.conf"
 	*.=emerg	*
 	EOF
-setup
+    setup -m0 >"${LOG2}" 2>&1
+}
 
+inject_logger()
+{
+    logger "Dummy message"
+    sleep 1			# Wait for any OS delays
+}
 
-print "TEST: Starting"
+inject_reload()
+{
+    reload
+    sleep 1			# Wait for any OS delays
+}
 
-#../src/logger -u "${SOCK}" ${MSG}
+verify_leaks()
+{
+    kill_pids
+    sleep 2			# Wait for syslogd to shut down
+    grep "All heap blocks were freed -- no leaks are possible" "$LOG2"
+}
 
-# Wait for any OS delays
-#sleep 1
-
-reload
-sleep 1
-
-OK
+run_step "Start basic syslogd under valgrind"  setup_valgrind
+run_step "Inject stimuli: logger"              inject_logger
+run_step "Inject stimuli: reload"              inject_reload
+run_step "Verify no leaks in valgrind output"  verify_leaks
