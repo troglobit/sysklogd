b37222aeb3e6e2a2ebd715b82604dd9506683a65
Joachim Wiberg <troglobit@gmail.com>
2024-12-29 16:47:00 +0100

    refactor unicode.sh to use run_step()
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/unicode.sh
+++ test/unicode.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 # Currently only same as local.sh but with unicode messages
 # From https://github.com/troglobit/sysklogd/issues/49
-# shellcheck disable=SC1090
 if [ -z "${srcdir}" ]; then
     srcdir=.
 fi
 . "${srcdir}/lib.sh"
 
-setup -8
-
 MSG="öäüÖÄÜß€¢§"
 MSG2="…‘’•"
 
-logger ${MSG}
-grep ${MSG} "${LOG}" || FAIL "Cannot find: ${MSG}"
+setup_unicode()
+{
+    setup -8 -m0
+}
 
-logger "${ALTSOCK}" ${MSG2}
-grep ${MSG2} "${LOG}" || FAIL "Cannot find: ${MSG2}"
+check_log_message()
+{
+    message="${!#}"           # Get last argument
+    [ $# -gt 1 ] && altsock="$1"
 
-OK
+    logger "${altsock}" "$message"
+    grep   "$message"   "$LOG"
+}
+
+run_step "Set up unicode capable syslogd" setup_unicode
+run_step "Verify logger"                  check_log_message "$MSG"
+run_step "Verify logger w/ alt. socket"   check_log_message "$ALTSOCK" "$MSG2"
