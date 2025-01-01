24e65fa43e147df8a8cd296ca860368cafa0513e
Joachim Wiberg <troglobit@gmail.com>
2025-01-01 15:43:45 +0100

    test: verify logger send to and syslogd multicast receive from group
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ test/multicast.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+. "${srcdir:-.}/lib.sh"
+
+GROUP=225.1.2.3
+
+setup_listen()
+{
+    ip link set lo up state up
+    ip route add default via 127.0.0.1
+
+    cat <<-EOF > "${CONF}"
+	*.*	$LOG
+	listen  $1:$PORT2
+	EOF
+    setup -m0
+}
+
+verify_listen()
+{
+    MSG="kilroy was here"
+
+    logger -h "$GROUP" -P "$PORT2" "${MSG}"
+    grep -H "${MSG}" "$LOG"
+}
+
+run_step "Setup daemon that listen to $GROUP"  setup_listen  "$GROUP"
+run_step "Verify sending to group $GROUP"      verify_listen "$GROUP"
