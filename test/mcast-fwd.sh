34391e74575451e9c24f41fb66030e082e7e90a9
Joachim Wiberg <troglobit@gmail.com>
2025-01-02 06:04:40 +0100

    test: verify forwarding to and reception from multicast group
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ test/mcast-fwd.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+. "${srcdir:-.}/lib.sh"
+
+MSG="Copying foobar to xyzzy"
+GROUP=225.1.2.3
+
+setup_sender()
+{
+    cat <<-EOF >"${CONF}"
+	*.*		@$GROUP	;RFC5424
+	EOF
+    setup -m0
+
+    ip route add default via 127.0.0.1
+}
+
+setup_receiver()
+{
+    cat <<-EOF > "${CONF2}"
+	uucp.info	$LOG2
+	listen		$GROUP
+	EOF
+    setup2 -m0 -nH
+}
+
+verify_mcast_fwd()
+{
+    cap_start
+    logger -p uucp.info "$MSG"
+    cap_stop
+
+    cap_find "$MSG"
+}
+
+verify_mcast_rcv()
+{
+    grep -H "$MSG" "$LOG2"
+}
+
+run_step "Set up sender syslogd"      setup_sender
+run_step "Set up receiver syslogd"    setup_receiver
+run_step "Verify multicast forward"   verify_mcast_fwd
+run_step "Verify multicast received"  verify_mcast_rcv
