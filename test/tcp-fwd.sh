dbf86a7325e6302c107ea7e6aabb78c435f92d9e
Joachim Wiberg <troglobit@gmail.com>
2026-03-20 18:27:08 +0100

    test: dog-food logger TCP and verbose mode in tcp-fwd
    Add two steps to the existing TCP forwarding test:
    
     - verify_direct_tcp: send directly from logger to the receiver's
       TCP listener using logger -h tcp://[::1]:PORT, bypassing the
       forwarding syslogd entirely.  Confirms the RFC 6587 framing and
       message delivery end-to-end.
    
     - verify_verbose_tcp: invoke logger -V and check that "connected"
       appears on stderr, exercising the new verbose mode added alongside
       the TCP transport feature.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/tcp-fwd.sh
+++ test/tcp-fwd.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Verify TCP forwarding between two syslogd instances
 #
-# Tests both @@ and tcp:// syntax for TCP forwarding.
+# Tests both @@ and tcp:// syntax for TCP forwarding, plus direct
+# logger-to-syslogd TCP delivery and the logger -V verbose mode.
 #
 . "${srcdir:-.}/lib.sh"
 
 MSG="tcp fwd test message"
 MSG2="tcp fwd url test message"
+MSG3="direct logger tcp message"
+MSG4="logger verbose tcp message"
 
 setup_receiver()
 {
@@ -62,8 +65,33 @@ verify_msg_url()
     grep "fwd - NTP123 - ${MSG2}" "${LOG2}"
 }
 
-run_step "Set up receiver syslogd with TCP listener"         setup_receiver_tcp
-run_step "Set up sender syslogd with @@ TCP forwarding"      setup_sender
-run_step "Verify TCP forward of message using @@ syntax"     verify_msg
-run_step "Reconfigure sender to use tcp:// URL syntax"       setup_sender_url
-run_step "Verify TCP forward of message using tcp:// syntax" verify_msg_url
+# Send directly from logger to the receiver's TCP listener, bypassing
+# the sender syslogd entirely.
+verify_direct_tcp()
+{
+    [ -x ../src/logger ] || SKIP 'logger missing'
+
+    ../src/logger -h "tcp://[::1]:${PORT2}" -t fwd -p ntp.notice "${MSG3}"
+    sleep 2
+
+    grep "${MSG3}" "${LOG2}"
+}
+
+# Use logger -V (verbose) to confirm that a TCP connection was established.
+# -V prints "connected to <host> port <N> (tcp)" on stderr on success.
+verify_verbose_tcp()
+{
+    [ -x ../src/logger ] || SKIP 'logger missing'
+
+    output=$(../src/logger -V -h "tcp://[::1]:${PORT2}" -t fwd -p ntp.notice "${MSG4}" 2>&1)
+    echo "${output}"
+    echo "${output}" | grep -i "connected"
+}
+
+run_step "Set up receiver syslogd with TCP listener"            setup_receiver_tcp
+run_step "Set up sender syslogd with @@ TCP forwarding"         setup_sender
+run_step "Verify TCP forward of message using @@ syntax"        verify_msg
+run_step "Reconfigure sender to use tcp:// URL syntax"          setup_sender_url
+run_step "Verify TCP forward of message using tcp:// syntax"    verify_msg_url
+run_step "Verify direct logger TCP send to syslogd listener"    verify_direct_tcp
+run_step "Verify logger -V reports TCP connection established"   verify_verbose_tcp
